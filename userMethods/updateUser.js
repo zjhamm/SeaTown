227a9edfcf0499c02e35c9c1b8689acbe19596fa
Donnie Ellstrom <donnie.ellstrom@freshconsulting.com>
2018-05-02 08:15:07 -0700

    Add updateUser method

---
+++ userMethods/updateUser.js
@@ -0,0 +1,93 @@
+var AWS = require("aws-sdk");
+var dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const userStatsTable = "UserStats";
+
+exports.handler = (event, context, callback) => {
+  let response = {
+    statusCode: 200,
+    headers: {},
+    body: "",
+    isBase64Encoded: false
+  };
+
+  let workoutName = "";
+  let personalRecord = "";
+  let notes = "";
+  let dateCompleted = "";
+  let expressionValues = {};
+
+  let updateExpression = "SET ";
+
+  if (event.queryPathParameters.workoutName) {
+    workoutName = event.queryPathParameters.workoutName;
+    // expressionValues["workoutName"] = workoutName;
+
+    if (event.queryPathParameters.personalRecord) {
+      personalRecord = event.queryPathParameters.personalRecord;
+      updateExpression += `${workoutName}.personalRecord = :personalRecord,`;
+      expressionValues[":personalRecord"] = personalRecord;
+    }
+    if (event.queryPathParameters.notes) {
+      notes = event.queryPathParameters.notes;
+      updateExpression += `${workoutName}.notes = :notes,`;
+      expressionValues[":notes"] = notes;
+    }
+    if (event.queryPathParameters.dateCompleted) {
+      dateCompleted = event.queryPathParameters.dateCompleted;
+      updateExpression += `${workoutName}.dateCompleted = :dateCompleted,`;
+      expressionValues[":dateCompleted"] = dateCompleted;
+    }
+  } else {
+    response["body"] = "WorkoutName was empty or null";
+    callback(null, response);
+  }
+
+  // var params = {
+  //     TableName: 'Image',
+  //     Key: {
+  //         Id: 'dynamodb.png'
+  //     },
+  //     UpdateExpression: 'SET address.province = :ma',
+  //     ConditionExpression: 'attribute_not_exists(address.province)',
+  //     ExpressionAttributeValues: {
+  //         ':ma': 'MA'
+  //     },
+  //     ReturnValues: 'ALL_NEW'
+  // };
+
+
+//   expressionValues = {
+//       ":wod": {
+//           "personalRecord": "12:40",
+//           "dateCompleted": "2018-05-01",
+//           "notes": "I DID IT!!!!"
+//       }
+//   }
+
+  const dynamoParams = {
+    TableName: userStatsTable,
+    Key: {
+      username: event.pathParameters.username
+    },
+    UpdateExpression: updateExpression.replace(/,\s*$/, ""),
+    //UpdateExpression: `SET ${workoutName} :wod`,
+    // ConditionExpression: `attribute_not_exists(${workoutName})`,
+    ExpressionAttributeValues: expressionValues,
+    ReturnValues: "UPDATED_NEW"
+  };
+
+  console.log("PARAMS: ", dynamoParams);
+
+  dynamodb.update(dynamoParams, function(err, data) {
+    if (err) {
+      response["body"] =
+        "Unable to update item. Error JSON: " + JSON.stringify(err, null, 2);
+      callback(null, response);
+    } else {
+      response["body"] =
+        "Successfully updated user! " + JSON.stringify(data, null, 2);
+      callback(null, response);
+    }
+  });
+};
